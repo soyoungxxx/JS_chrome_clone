d0d0fa086ffd08d40f4a4080d8dc79adac3c1119
soyoungxxx <otherplet@gmail.com>
2023-05-31 21:22:34 +0900

    form

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const loginInput = document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
 
 function onLoginBtnClick() {
-    console.log(loginInput.value);
+    const username = loginInput.value;
+    if (username == "") {
+        alert("Please write your name");
+    } else if(username)
 }
 
 loginButton.addEventListener("click", onLoginBtnClick);
