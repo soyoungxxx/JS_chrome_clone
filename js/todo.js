37187264f9d27e1936bbd6d8817f6a015dcc0b36
soyoungxxx <otherplet@gmail.com>
2023-06-07 20:18:51 +0900

    deleteing todos part one

--- js/todo.js
+++ js/todo.js
@@ -7,8 +7,8 @@ const TODOS_KEY = "todos"
 let toDos = [];
 
 function deleteTodo(event) {
-    const li = (event.target.parentElement);
-    li.remove();
+    const li = (event.target.parentElement.id);
+    console.log(li);
 }
 
 function saveToDos() {
@@ -17,13 +17,16 @@ function saveToDos() {
 
 function paintToDo(newTodo) {
     const li = document.createElement("li");
+    li.id = newTodo.id;
+
     const span = document.createElement("span");
+    span.innerText = newTodo.text;
+
     const button = document.createElement("button");
     button.innerText = "❤️";
     button.addEventListener("click", deleteTodo);
     li.appendChild(span);
     li.appendChild(button);
-    span.innerText = newTodo;
     toDoList.appendChild(li);
 }
 
@@ -31,8 +34,12 @@ function handleToDoSubmit(event) {
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = "";
-    toDos.push(newTodo);
-    paintToDo(newTodo);
+    const newTodoObj = {
+        text:newTodo,
+        id:Date.now()
+    };
+    toDos.push(newTodoObj);
+    paintToDo(newTodoObj);
     saveToDos();
 }
 
